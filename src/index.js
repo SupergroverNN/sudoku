2d8d0075750933723d4da9cc3038f2b9201f9670
shipulinia <shipulinia@lad24.ru>
2019-10-21 12:31:13 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -6,30 +6,32 @@ function checkSector(board, row, col, value) {
   for (let i = 0; i < 9; i++) {
     const newRow = 3 * Math.floor(row / 3) + Math.floor(i / 3);
     const newCol = 3 * Math.floor(col / 3) + (i % 3);
-    if (board[newRow, newCol] === value) {
+    if (board[newRow][newCol] === value) {
       return false;
     }
-    return true;
   }
+  return true;
 }
 function checkVerticalHorizontal(board, row, col, value) {
   for (let i = 0; i < 9; i++) {
     if (board[i][col] === value || board[row][i] === value) {
-      return false;;
+      return false;
     }
   }
   return true;
 }
 function chechValue(board, row, col, value) {
-  return checkSector(board, row, col, value) && checkVerticalHorizontal(board, row, col, value)
+  return checkVerticalHorizontal(board, row, col, value) && checkSector(board, row, col, value);
 }
 
 function solve(data) {
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
       if (data[i][j] === 0) {
-        for (let value = 1; value <= 9; value++) { // перебираем все значения от 1 до 9 для данной ячейки
-          if (chechValue(data, i, j, value)) { // если все норм то записываем данное значение
+        for (let value = 1; value <= 9; value++) {
+          // перебираем все значения от 1 до 9 для данной ячейки
+          if (chechValue(data, i, j, value)) {
+            // если все норм то записываем данное значение
             data[i][j] = value;
             if (solve(data)) {
               return true;
