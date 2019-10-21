f6b85e2ea7fb75c2468b765d60957c4e933ce08e
shipulinia <shipulinia@lad24.ru>
2019-10-21 12:18:24 +0300

    feat: add functionality to pass 1 test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  solve(matrix);
+  return matrix;
+};
+function checkSector(board, row, col, value) {
+  for (let i = 0; i < 9; i++) {
+    const newRow = 3 * Math.floor(row / 3) + Math.floor(i / 3);
+    const newCol = 3 * Math.floor(col / 3) + (i % 3);
+    if (board[newRow, newCol] === value) {
+      return false;
+    }
+    return true;
+  }
+}
+function checkVerticalHorizontal(board, row, col, value) {
+  for (let i = 0; i < 9; i++) {
+    if (board[i][col] === value || board[row][i] === value) {
+      return false;;
+    }
+  }
+  return true;
+}
+function chechValue(board, row, col, value) {
+  return checkSector(board, row, col, value) && checkVerticalHorizontal(board, row, col, value)
+}
+
+function solve(data) {
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (data[i][j] === 0) {
+        for (let value = 1; value <= 9; value++) { // перебираем все значения от 1 до 9 для данной ячейки
+          if (chechValue(data, i, j, value)) { // если все норм то записываем данное значение
+            data[i][j] = value;
+            if (solve(data)) {
+              return true;
+            } else {
+              data[i][j] = 0;
+            }
+          }
+        }
+        return false;
+      }
+    }
+  }
+  return true;
 }
